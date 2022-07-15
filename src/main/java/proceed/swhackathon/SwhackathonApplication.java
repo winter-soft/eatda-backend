efd6a0780ea6750deec340b7b1b3aaebc62e7e40
ghdcksgml1 <ghdcksgml2@naver.com>
2022-07-16 01:01:05 +0900

    dev setting

---
+++ src/main/java/proceed/swhackathon/SwhackathonApplication.java
@@ -0,0 +1,13 @@
+package proceed.swhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwhackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwhackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proceed/swhackathon/SwhackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package proceed.swhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwhackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
