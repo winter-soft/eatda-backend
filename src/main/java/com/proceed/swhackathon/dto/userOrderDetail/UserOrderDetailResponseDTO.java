15ad2403e313c95f04409ca52f27297b01b13207
ghdcksgml1 <ghdcksgml2@naver.com>
2022-11-13 01:33:59 +0900

    refactor : #35 - 주문목록 조회 api Order필드 추가

--- src/main/java/com/proceed/swhackathon/dto/userOrderDetail/UserOrderDetailResponseDTO.java
+++ src/main/java/com/proceed/swhackathon/dto/userOrderDetail/UserOrderDetailResponseDTO.java
@@ -1,5 +1,6 @@
 package com.proceed.swhackathon.dto.userOrderDetail;
 
+import com.proceed.swhackathon.model.Order;
 import com.proceed.swhackathon.model.OrderDetail;
 import com.proceed.swhackathon.model.Store;
 import com.proceed.swhackathon.model.UserOrderDetail;
@@ -18,6 +19,7 @@ public class UserOrderDetailResponseDTO {
     private Long id;
     private int totalPrice;
     private Store store;
+    private Order order;
     private List<OrderDetail> orderDetails;
 
     public static UserOrderDetailResponseDTO entityToDTO(UserOrderDetail uod){
@@ -25,6 +27,7 @@ public class UserOrderDetailResponseDTO {
                 .id(uod.getId())
                 .totalPrice(uod.getTotalPrice())
                 .store(uod.getOrder().getStore())
+                .order(uod.getOrder())
                 .orderDetails(uod.getOrderDetails())
                 .build();
     }
