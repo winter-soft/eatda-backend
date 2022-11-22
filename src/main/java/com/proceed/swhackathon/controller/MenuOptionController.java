13f7b1f1a063015283269f3811a2ec304797281d
ghdcksgml1 <ghdcksgml2@naver.com>
2022-11-22 14:04:02 +0900

    feat : MenuOption Controller 추가

---
+++ src/main/java/com/proceed/swhackathon/controller/MenuOptionController.java
@@ -0,0 +1,29 @@
+package com.proceed.swhackathon.controller;
+
+import com.proceed.swhackathon.dto.ResponseDTO;
+import com.proceed.swhackathon.repository.MenuOptionRepository;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/menuOption")
+@RequiredArgsConstructor
+public class MenuOptionController {
+    private final MenuOptionRepository menuOptionRepository;
+
+    @GetMapping("/{menuId}")
+    @ApiOperation(value = "메뉴에 맞는 옵션값을 반환한다.", notes = "menuId를 받아 옵션값을 리턴")
+    public ResponseDTO<?> selectMenuOption(@PathVariable Long menuId){
+        Map<String, Object> m = new HashMap<>();
+        m.put("menuOptions", menuOptionRepository.findAllByMenu_id(menuId));
+
+        return new ResponseDTO<>(HttpStatus.OK.value(), m);
+    }
+}
--- src/main/java/com/proceed/swhackathon/repository/MenuOptionRepository.java
+++ src/main/java/com/proceed/swhackathon/repository/MenuOptionRepository.java
@@ -2,7 +2,12 @@ package com.proceed.swhackathon.repository;
 
 import com.proceed.swhackathon.model.MenuOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MenuOptionRepository extends JpaRepository<MenuOption, Long> {
 
+    @Query("select m from MenuOption m where m.menu_id = :menu_id")
+    public List<MenuOption> findAllByMenu_id(Long menu_id);
 }
