a530ed2ebb1d0f9cce52dfccd4451deae87ce7c1
ghdcksgml1 <ghdcksgml2@naver.com>
2023-01-15 14:44:34 +0900

    remove: 스케쥴러 코드 기존 서비스 클래스에 삽입

--- src/main/java/com/proceed/swhackathon/service/OrderDetailService.java
+++ src/main/java/com/proceed/swhackathon/service/OrderDetailService.java
@@ -21,6 +21,7 @@ import com.proceed.swhackathon.model.*;
 import com.proceed.swhackathon.repository.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -299,6 +300,7 @@ public class OrderDetailService {
     }
 
     @Transactional
+    @Scheduled(cron = "0 0 1 * * *")
     public void garbageCollectOrderDetail(){
         List<OrderDetail> ods = orderDetailRepository.deleteAllByMenuCheckIsFalseAndUserOrderDetailIsNull();
         for(OrderDetail od : ods){
